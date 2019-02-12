75e7d9eccd8b0cc6f12eb3473e4f6ee065e4c944
Andrey <xamidylin@gismeteo.com>
2019-02-12 15:26:49 +0300

    fix(router): router init

--- app/router.ts
+++ app/router.ts
@@ -1,4 +1,4 @@
-import createRouter, {Options, Route, RouteNode, Router as Router5} from 'router5';
+import createRouter, {Options, Route, RouteNode, Router as Router5, State as RouterState} from 'router5';
 import * as browserPlugin from 'router5-plugin-browser/dist';
 import {Observable, startWith, tap} from "@hypertype/core";
 
@@ -8,21 +8,8 @@ export class IRouterOptions {
 }
 
 export class Router {
+    public State$: Observable<RouterState>;
     private router: Router5;
-
-    constructor(routerInit: IRouterOptions){
-        this.router = createRouter(routerInit.routes, routerInit.options);
-        this.router.usePlugin(browserPlugin());
-        this.router.start();
-    }
-
-    public State$ = new Observable(subscr => {
-        this.router.subscribe(change => subscr.next(change.route))
-    }).pipe(
-        startWith(this.router.getState()),
-        tap(console.warn)
-    );
-
     public Actions: {
         [K in keyof IRouteActions]: (...args: any[]) => void;
     } = {
@@ -30,6 +17,18 @@ export class Router {
             console.log(route)
             this.router.navigate(route);
         }
+    };
+
+    constructor(routerInit: IRouterOptions) {
+        this.router = createRouter(routerInit.routes, routerInit.options);
+        this.router.usePlugin(browserPlugin());
+        this.router.start();
+        this.State$ = new Observable(subscr => {
+            this.router.subscribe(change => subscr.next(change.route))
+        }).pipe(
+            startWith(this.router.getState()),
+            tap(console.warn)
+        );
     }
 }
 
